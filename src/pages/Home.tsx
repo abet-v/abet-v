29d05e9af4051d54458765f9cfc8c0876975be3c
Vincent <vincentabet@gmail.com>
2023-11-17 14:09:29 +0100

    test modif

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,83 +14,73 @@ import rocketIcon from "assets/icons/rocket-chat.png";
 import tchapIcon from "assets/icons/tchap.png";
 
 export const Home = memo(() => {
-	const { t } = useTranslation({ Home });
-	return (
-		<>
-			<GlHero
-				title={t("heroTitle")}
-				subTitle={t("heroSubtitle")}
-				illustration={{
-					"type": "image",
-					"src": heroPng,
-					"hasShadow": false
-				}}
-				hasLinkToSectionBellow={true}
-			/>
+  const { t } = useTranslation({ Home });
+  return (
+    <>
+      <h1>HELLOOO</h1>
+      <GlHero
+        title={t("heroTitle")}
+        subTitle={t("heroSubtitle")}
+        illustration={{
+          type: "image",
+          src: heroPng,
+          hasShadow: false,
+        }}
+        hasLinkToSectionBellow={true}
+      />
 
-			<GlArticle
-				title={t("articleTitle")}
-				body={t("articleBody")}
-				buttonLabel={t("articleButtonLabel")}
-				buttonLink={{
-					"href": "https://example.com",
-				}}
-				illustration={{
-					"type": "image",
-					"src": articlePng,
-					"hasShadow": false
-				}}
-				hasAnimation={true}
-				illustrationPosition="left"
-			/>
+      <GlArticle
+        title={t("articleTitle")}
+        body={t("articleBody")}
+        buttonLabel={t("articleButtonLabel")}
+        buttonLink={{
+          href: "https://example.com",
+        }}
+        illustration={{
+          type: "image",
+          src: articlePng,
+          hasShadow: false,
+        }}
+        hasAnimation={true}
+        illustrationPosition="left"
+      />
 
-			<GlCards>
-				<GlLogoCard
-					title={t("card1Title")}
-					paragraph={t("card1Paragraph")}
-					buttonLabel="Button Label"
-					iconUrls={[
-						tchapIcon,
-						githubIcon
+      <GlCards>
+        <GlLogoCard
+          title={t("card1Title")}
+          paragraph={t("card1Paragraph")}
+          buttonLabel="Button Label"
+          iconUrls={[tchapIcon, githubIcon]}
+        />
+        <GlLogoCard
+          title={t("card2Title")}
+          paragraph={t("card2Paragraph")}
+          buttonLabel="Button Label"
+          iconUrls={[rocketIcon]}
+        />
 
-					]}
-				/>
-				<GlLogoCard
-					title={t("card2Title")}
-					paragraph={t("card2Paragraph")}
-					buttonLabel="Button Label"
-					iconUrls={[
-						rocketIcon
-					]}
-				/>
-
-				<GlLogoCard
-					title={t("card3Title")}
-					paragraph={t("card3Paragraph")}
-					buttonLabel="Button Label"
-					iconUrls={[
-						balloonIcon,
-						drawioIcon,
-						rocketIcon,
-						plusIcon
-					]}
-					overlapIcons={true}
-				/>
-			</GlCards>
-		</>
-	);
+        <GlLogoCard
+          title={t("card3Title")}
+          paragraph={t("card3Paragraph")}
+          buttonLabel="Button Label"
+          iconUrls={[balloonIcon, drawioIcon, rocketIcon, plusIcon]}
+          overlapIcons={true}
+        />
+      </GlCards>
+    </>
+  );
 });
 
 export const { i18n } = declareComponentKeys<
-	| "heroTitle"
-	| "heroSubtitle"
-	| "articleTitle"
-	| "articleBody"
-	| "articleButtonLabel"
-	| "card1Title"
-	| "card2Title"
-	| "card3Title"
-	| "card1Paragraph"
-	| "card2Paragraph"
-	| "card3Paragraph"
+  | "heroTitle"
+  | "heroSubtitle"
+  | "articleTitle"
+  | "articleBody"
+  | "articleButtonLabel"
+  | "card1Title"
+  | "card2Title"
+  | "card3Title"
+  | "card1Paragraph"
+  | "card2Paragraph"
+  | "card3Paragraph"
 >()({ Home });
